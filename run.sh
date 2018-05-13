a33ea8b616c7faabef46b1067c5f3c140ee59092
Ying Huang <yhuang@imvu.com>
2018-05-13 14:42:51 -0700

    n

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-nvidia-docker-compose -f ./docker-compose.yml up --build --force-recreate
+docker-compose -f ./docker-compose.yml up --build --force-recreate
